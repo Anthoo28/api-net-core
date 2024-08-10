708a9434be6be7a9c3805227df75e0214a18dcc2
anthoo28 <zarek1404@gmail.com>
2024-08-10 02:39:51 -0500

    init commit

--- obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb97debcfaf4d8b417e48dced450d5058c0d17d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dd18ec2d762e3280a6755bce7ecdddaf4ab643c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
